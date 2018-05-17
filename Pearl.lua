c419bcc5d35a921a7daa322227f0a27672084521
Stupefying <349748574@gapps.yrdsb.ca>
2018-05-17 16:24:52 -0400

    Update Pearl.lua

--- Pearl.lua
+++ Pearl.lua
@@ -9,9 +9,9 @@ function Module:GET (URL, NoCache, Headers)
   -- Paramater Type Checking --
   if typeof(URL) ~= "String" then 
     return '@PARAM "URL" Is Not A String'
-  elseif typeof(NoCache) !== "Boolean" then
+  elseif typeof(NoCache) ~= "Boolean" then
     return '@PARAM "NoCache" Is Not A Boolean'
-  elseif typeof(Headers) !== "Boolean" and typeof(Headers) !== "Table" then
+  elseif typeof(Headers) `= "Boolean" and typeof(Headers) !== "Table" then
     return '@PARAM "Headers" Is Not A Boolean And/Or a Table'
   end
   
