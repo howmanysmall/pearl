05ef6dc497287c9f722ee175acaf1189cbf7c0e0
Stupefying <349748574@gapps.yrdsb.ca>
2018-05-17 16:05:36 -0400

    Update Http.lua

--- Http.lua
+++ Http.lua
@@ -1,2 +1,18 @@
 -- Declare Service(s) --
 local HttpService = game:GetService("HttpService");
+
+-- Declare Variable(s) --
+local Module = {};
+
+-- GET Request Function --
+local Module:GET = function ()
+
+end
+  
+-- POST Request Function --
+local Module:POST = function ()
+
+end
+
+-- Export/Return Function(s) --
+return Module;
