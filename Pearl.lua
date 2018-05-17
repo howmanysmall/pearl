63e554ff8ab47a289a0a39d562b5b46d2500554e
Stupefying <349748574@gapps.yrdsb.ca>
2018-05-17 16:21:53 -0400

    Update Pearl.lua

--- Pearl.lua
+++ Pearl.lua
@@ -5,12 +5,21 @@ local HttpService = game:GetService("HttpService");
 local Module = {};
 
 -- GET Request Function --
-local Module:GET = function ()
+Module:GET = function (URL, NoCache, Headers)
+  -- Paramater Type Checking --
+  if typeof(URL) !== "String" then 
+    return '@PARAM "URL" Is Not A String'
+  elseif typeof(NoCache) !== "Boolean" then
+    return '@PARAM "NoCache" Is Not A Boolean'
+  elseif typeof(Headers) !== "Boolean" and typeof(Headers) !== "Table" then
+    return '@PARAM "Headers" Is Not A Boolean And/Or a Table'
+  end
+  
 
 end
   
 -- POST Request Function --
-local Module:POST = function ()
+Module:POST = function (URL, Data, ContentType, Compress, Headers)
 
 end
 
